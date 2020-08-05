5c1fb43def7513a1c81b589266643870d7390639
Unipomp Team <unipomp.team@protonmail.com>
2020-08-05 14:30:22 +0300

    fix name

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -6,8 +6,8 @@ import './libraries/SafeMath.sol';
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Unipomp';
-    string public constant symbol = 'UNIPO';
+    string public constant name = 'Unipomp LP';
+    string public constant symbol = 'POMP-LP';
     uint8 public constant decimals = 18;
     uint  public totalSupply;
     mapping(address => uint) public balanceOf;
